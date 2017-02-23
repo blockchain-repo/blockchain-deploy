bd67a982c2688a02eb63a1306b18fe8861a44b65
wangxin <wx_cs_db_88@163.com>
2017-02-23 15:58:57 +0800

    fixed the monitor error and modify the rep for docker images

--- deploy/script/setup_monitor.sh
+++ deploy/script/setup_monitor.sh
@@ -3,11 +3,13 @@ echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/source
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-get update
 apt-get -y --force-yes install docker-engine
-wget -O /usr/local/bin/docker-compose https://get.daocloud.io/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m`
+
+pip3 install -U docker-compose==1.8.0
+
+#pip3 install -U docker-compose
 #wget -O /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m`
 # curl -L "https://github.com/docker/compose/releases/download/1.8.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#curl -L https://get.daocloud.io/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-INFLUXDB_DATA=/monitor/data INIT_SCRIPT=$PWD/init_script.influxql docker-compose -f ../../monitor/docker-compose-monitor.yml up
-# INFLUXDB_DATA=/monitor/data INIT_SCRIPT=$PWD/init_script.influxql docker-compose -f ../../monitor/docker-compose-monitor-dev.yml up
+INFLUXDB_DATA=/monitor/data INIT_SCRIPT=$PWD/init_script.influxql docker-compose -f ../../docker-compose-monitor.yml up
+# INFLUXDB_DATA=/monitor/data INIT_SCRIPT=$PWD/init_script.influxql docker-compose -f ../../docker-compose-monitor-dev.yml up
