4e309088661cc5f88a889114afc14b78fdf1f2d5
wangxin <wx_cs_db_88@163.com>
2017-02-24 17:27:55 +0800

    temp added test shell for restore ops

---
+++ deploy/script/test_clear_unichain.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# The set -e option instructs bash to immediately exit
+# if any command has a non-zero exit status
+set -e
+
+echo -e "[INFO]==========stop unichain=========="
+fab stop_unichain
+
+echo -e "[INFO]==========stop unichain_restore=========="
+fab stop_unichain_restore
+
+echo -e "[INFO]==========clear and init unichain=========="
+fab init_unichain:shards=True,replicas=True
