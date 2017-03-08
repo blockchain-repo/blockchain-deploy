006f51ca32127a1fb62e1c20c443bf8b9ab4e735
bc1 <bc1@bc1.localdomain>
2017-03-08 16:53:56 +0800

    modify install_docker_pip3.sh

--- deploy/script/monitor_script/install_docker_pip3.sh
+++ deploy/script/monitor_script/install_docker_pip3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo -e "[INFO]==========begin install pip3=========="
+apt-get install python3-pip
+echo -e "[INFO]==========Done=========="
+
+
 echo -e "[INFO]==========begin install docker=========="
 echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
