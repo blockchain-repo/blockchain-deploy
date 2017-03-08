f095537da585ff2dc1a130cf71aaf6aa583e688d
wsp_work <wsp_work@163.com>
2017-03-08 03:07:12 -0500

    modify load_images.sh

--- deploy/script/monitor_script/load_images.sh
+++ deploy/script/monitor_script/load_images.sh
@@ -6,3 +6,8 @@ echo -e "[INFO]==========begin load "$i" images=========="
 docker load --input $i
 done
 echo -e "[INFO]==========Done=========="
+
+echo -e "[INFO]==========images list=========="
+docker images
+echo -e "[INFO]==========Done=========="
+
