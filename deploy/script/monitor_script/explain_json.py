c13a1c8075305ebf02ad357563d5cdc2aa027831
wsp_work <wsp_work@163.com>
2017-03-10 16:20:14 +0800

    add grafana json temple

--- deploy/script/monitor_script/explain_json.py
+++ deploy/script/monitor_script/explain_json.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-\n
 import json
 import os.path
+
+import sys
 from query_condtion import get_all_condtions,get_all_business
 from file_utils import find_file,chang_dir
 
-def read_jsonFile():
+def read_jsonFile(fileNum):
 
     all_conditions = get_all_business()
     condtions_length = len(all_conditions)
 
-    conf_filename = 'hardware.json'
+    conf_filename = choose_jsonFile(fileNum)
     file_path = './grafana_temple'
     old_cwd = os.getcwd()
     json_files_path = find_file(conf_filename,file_path)
@@ -25,11 +27,9 @@ def read_jsonFile():
     chang_dir(old_cwd)
     return newDic
 
-
-
-def write_jsonFile():
-    newDic = read_jsonFile()
-    conf_filename = 'hardware.json'
+def write_jsonFile(fileNum):
+    newDic = read_jsonFile(fileNum)
+    conf_filename = choose_jsonFile(fileNum)
     file_path = '../../../grafana-unichain-docker/grafana_json'
     old_cwd = os.getcwd()
     json_files_path = find_file(conf_filename,file_path)
@@ -38,8 +38,19 @@ def write_jsonFile():
         json.dump(newDic, w)
     chang_dir(old_cwd)
 
+def choose_jsonFile(fileNum):
+    if fileNum == 1:
+        return 'uni-ledger-business.json'
+    elif fileNum == 2:
+        return 'uni-ledger-hardware2.json'
+#    elif fileNum == 3:
+#        return 'uni-ledger-hardware1.json'
+
 if __name__ == '__main__':
-    write_jsonFile()
+
+    write_jsonFile(1)
+    write_jsonFile(2)
+
 
 
 
