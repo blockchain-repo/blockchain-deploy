aca0100c61a3249af749c1bb793bb160db822ab4
wsp_work <wsp_work@163.com>
2017-03-13 19:38:11 -0700

    add install_docker_local.sh

---
+++ monitor_deploy/script/install_docker_local.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo -e "[INFO]==========Decompression tar package=========="
+tar -xvf deb.tar
+echo -e "[INFO]==========Done=========="
+echo -e "  "
+
+echo -e "[INFO]==========begin install git=========="
+cd deb/git
+dpkg -i *.deb
+git
+echo -e "[INFO]==========Done=========="
+echo -e "  "
+
+echo -e "[INFO]==========begin install pip3=========="
+cd ../pip3
+dpkg -i *.deb
+pip3
+echo -e "[INFO]==========Done=========="
+echo -e "  "
+
+echo -e "[INFO]==========begin install docker=========="
+cd ../docker
+dpkg -i *.deb
+docker
+echo -e "[INFO]==========Done=========="
+echo -e "  "
+
+echo -e "[INFO]==========begin install docker-compose=========="
+echo -e "    "
+echo -e "[INFO]==========begin copy python packages=========="
+cd ../docker_compose_package/dist-packages
+cp -r ./ /usr/local/lib/python3.4/dist-packages
+cd ../../
+cp docker-compose /usr/local/bin
+chmod +x /usr/local/bin/docker-compose
+cd ..
+docker-compose
+echo -e "[INFO]==========Done=========="
+echo -e "  "
+
