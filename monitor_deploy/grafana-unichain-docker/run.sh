62f440bbd7d4d8f0a984ef648802d2deb4f57bec
wsp_work <wsp_work@163.com>
2017-03-14 20:02:51 -0700

    modify run.sh and grafana_json

--- monitor_deploy/grafana-unichain-docker/run.sh
+++ monitor_deploy/grafana-unichain-docker/run.sh
@@ -8,6 +8,7 @@ fi
 
 if [ -n "${INFLUXDB_HOST}" ]; then
     sed -i -u "s#const influxdb_host = .*#const influxdb_host = \'${INFLUXDB_HOST}\';#g" ${DASHBOARD_SCRIPT}
+    sed -i -u "s#const influxdb_host = .*#const influxdb_host = \'${INFLUXDB_HOST}\';#g" ${DASHBOARD_SCRIPT}
 fi
 
 connect_db() {
@@ -16,7 +17,8 @@ connect_db() {
         echo "$(date) - waiting for Grafana to be ready"
         sleep 5
     done
-    curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name": "influx", "type": "influxdb","url": "http://'"${INFLUXDB_HOST}"':8086", "access": "proxy", "isDefault": true, "database": "telegraf", "user": "root", "password":"root"}'
+    curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name": "influx", "type": "influxdb","url": "http://'"${INFLUXDB_HOST}"':8086", "access": "proxy", "isDefault": false, "database": "telegraf", "user": "root", "password":"root"}'
+    curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name": "collectd", "type": "influxdb","url": "http://'"${INFLUXDB_HOST}"':8086", "access": "proxy", "isDefault": false, "database": "collectdb", "user": "root", "password":"root"}'
 }
 
 connect_db &
